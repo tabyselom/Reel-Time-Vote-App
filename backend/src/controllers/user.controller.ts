0e935b909f8f1b9ad1fb637ec68d08975d5ea1fb
tabyselom <abyselom111@gmail.com>
2025-08-16 18:35:42 +0300

    modified:   backend/package-lock.json modified:   backend/package.json 	modified:   backend/src/controllers/poll.controller.ts modified:   backend/src/controllers/user.controller.ts

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { generateToken } from "../lib/utility";
 import bcrypt from "bcryptjs";
 import { user } from "../types/user.type";
