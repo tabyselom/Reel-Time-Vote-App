d1bd8b2c7584d504be9866e1be286b9be2130aac
tabyselom <abyselom111@gmail.com>
2025-08-17 12:57:37 +0300

    modifie

--- backend/src/lib/utility.ts
+++ backend/src/lib/utility.ts
@@ -10,6 +10,7 @@ export const generateToken = (userId: string, res: Response) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_DEV !== "development",
+    sameSite: "none",
   });
 
   return token;
