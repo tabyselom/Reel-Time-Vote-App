57f64c5d1c19af0e58700c9db5c5be7e34c2c3fc
tabyselom <abyselom111@gmail.com>
2025-08-17 12:15:05 +0300

    modifie

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/api"
+    : process.env.NEXT_PUBLIC_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL,
